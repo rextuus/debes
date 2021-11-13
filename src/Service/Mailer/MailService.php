25dbf17b3dd37b2ed76259b39c2ff932a6ce94a9
rextuus <rextuus@gmail.com>
2021-11-13 20:12:55 +0100

    Some changes

--- src/Service/Mailer/MailService.php
+++ src/Service/Mailer/MailService.php
@@ -2,6 +2,9 @@
 
 namespace App\Service\Mailer;
 
+use App\Entity\Transaction;
+use App\Entity\User;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mailer\MailerInterface;
 
 /**
@@ -12,6 +15,12 @@ use Symfony\Component\Mailer\MailerInterface;
  */
 class MailService
 {
+
+    private const DEBES_MAIL_ADDRESS = 'debes@wh-company.de';
+    private const MAIL_DEBT_CREATED = 'debt_created';
+    private const MAIL_DEBT_ACCEPTED = 'debt_accepted';
+    private const MAIL_DEBT_ = 'debt_accepted';
+
     /**
      * @var MailerInterface
      */
@@ -25,7 +34,42 @@ class MailService
         $this->mailer = $mailer;
     }
 
-    public function sendNotificationMail(){
+    public function sendNotificationMail(User $user, Transaction $transaction, string $mailVariant){
+        $receiver = $transaction->getDebts()[0]->getOwner();
 
+        $subject = '';
+        $text = '';
+        $template = '';
+        if ($mailVariant === self::MAIL_NEW_DEBT){
+            $text = 'Es gibt leider schlechte Nachrichten. Jemand hat eine neue Schuldlast für deinen Debes-Account hinterlegt';
+            $subject = 'Du hast neue Schulden gemacht';
+            $template = 'mailer/mail.created.html.twig';
+        }elseif ($mailVariant === self::MAIL_NEW_DEBT){
+            $text = 'Es gibt leider schlechte Nachrichten. Jemand hat eine neue Schuldlast für deinen Debes-Account hinterlegt';
+            $subject = 'Du hast neue Schulden gemacht';
+            $template = 'mailer/mail.created.html.twig';
+        }
+
+        $problems = 0;
+        $transactions = 0;
+        $debts = 0;
+
+        $email = (new TemplatedEmail())
+            ->from(self::DEBES_MAIL_ADDRESS)
+            ->to($receiver->getEmail())
+            ->subject($subject)
+            ->htmlTemplate($template)
+            ->context([
+                'userName' => $receiver->getFirstName(),
+                'text' => $text,
+                'loaner' => $transaction->getLoans()[0]->getOwner()->getFirstName(),
+                'reason' => $transaction->getReason(),
+                'amount' => $transaction->getAmount(),
+                'problems' => $problems,
+                'transactions' => $transactions,
+                'debts' => $debts,
+            ]);
+
+        $this->mailer->send($email);
     }
 }
